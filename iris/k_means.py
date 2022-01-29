36b66f86ef648569aa079be528fca51ae0bcefe0
vicrobot <chaosplusorder@gmail.com>
2022-01-30 01:14:58 +0530

    Remove train test split as it is not needed in mere grouping

--- iris/k_means.py
+++ iris/k_means.py
@@ -2,31 +2,25 @@
 import pandas as pd
 from numpy import inf
 import numpy as np
-from sklearn.model_selection import train_test_split as tts
 from scipy.spatial.distance import euclidean
 #from itertools import product as prd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation as fa
 
 
-
-#loading data, making test train split
+#loading data
 df = pd.read_csv('iris.csv')
-df_train, df_test = tts(df, test_size=0.2, train_size = 0.8) #defaultly, train is 0.75 and test is 0.25
 
-X_train = df_train[df_train.columns[:-1]]
-Y_train = df_train[df_train.columns[-1]]
-
-X_test = df_test[df_test.columns[:-1]]
-Y_test = df_test[df_test.columns[-1]]
+X = df[df.columns[:-1]]
+Y = df[df.columns[-1]]
 
 k = 3
-X_train_n = X_train.to_numpy()
-l_X_train_n = list(X_train_n)
-k_means = X_train_n[:k]
+X_n = X.to_numpy()
+l_X_n = list(X_n)
+k_means = X_n[:k]
 
 M = 20 #iterations for k _means
-retries_c = 5
+retries_c = 5 #have to make this much retries to overcome from the loss from sticking in local minima
 
 
 """
@@ -37,10 +31,10 @@ This fight helps k-means to minimize when right means are obtained (hopefully)
 def gen_k_means(k_means):
     #iterating M times
     for _ in range(M):
-        #X_train_runner stores corresponding neta of that value
-        X_train_runner = []
+        #X_runner stores corresponding neta of that value
+        X_runner = []
         color_code_l = []
-        for value in X_train_n:
+        for value in X_n:
             chosen_mean = k_means[0]
             dist = inf
             color_code = 0
@@ -55,11 +49,11 @@ def gen_k_means(k_means):
                     color_code = counter
                 counter += 1
             color_code_l.append(color_code)
-            X_train_runner.append(tuple(chosen_mean))
+            X_runner.append(tuple(chosen_mean))
 
         #for chosen neta and their corresponding public, this below stuff makes new election and new means
         #are produced(intra mean)
-        df2 = pd.DataFrame({'vals':l_X_train_n, 'neta':X_train_runner})
+        df2 = pd.DataFrame({'vals':l_X_n, 'neta':X_runner})
         grouped = df2.groupby('neta')
         k_means = (grouped.sum()/grouped.count()).to_numpy().flatten()
         yield k_means, color_code_l, _
@@ -68,8 +62,8 @@ def gen_k_means(k_means):
 class anim:
     def __init__(self):
         self.fig, (self.ax1, self.ax2) = plt.subplots(1,2, figsize=(20,10))
-        self.scat1 = self.ax1.scatter(X_train_n[:,0],X_train_n[:,2], c='y')
-        self.scat2 = self.ax2.scatter(X_train_n[:,1],X_train_n[:,3], c='y')
+        self.scat1 = self.ax1.scatter(X_n[:,0],X_n[:,2], c='y')
+        self.scat2 = self.ax2.scatter(X_n[:,1],X_n[:,3], c='y')
         self.colors = {0:'r', 1:'g', 2:'b'}
         self.ax1.set_xlabel('Iris sepal length')
         self.ax1.set_ylabel('Iris petal length')
@@ -79,8 +73,8 @@ class anim:
     def animate(self,j):
         k_means, color_code, iter_n = j
         self.k_means = k_means
-        main_frame1 = X_train_n[:, [0,2]]
-        main_frame2 = X_train_n[:, [1,3]]
+        main_frame1 = X_n[:, [0,2]]
+        main_frame2 = X_n[:, [1,3]]
         plt.suptitle(f'Frame: {iter_n+1}')
         color = [self.colors[i] for i in color_code]
         frame_2_1 = []
@@ -98,7 +92,7 @@ class anim:
         self.scat2.set_offsets(n_mf2)  #sets coords for points to plot
 
     def run(self,gen_k_means):
-        ani = fa(self.fig, self.animate, frames = gen_k_means(X_train_n), interval = 500,
+        ani = fa(self.fig, self.animate, frames = gen_k_means(X_n), interval = 500,
                         blit = False, repeat = False)
 
         plt.show()
